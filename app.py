0cfd211f385ca8b76bdafcb78937eb4b4d142ea7
waleolajumoke <waleolajumoke@gmail.com>
2024-09-10 14:32:15 +0100

    modified the host port

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_world():
     return 'welcome to Python'
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0',port=5000)
     
 # run the app with python app.py
 # go to http://127.0.0.1:5000/ to see the output
