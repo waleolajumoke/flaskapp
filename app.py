2e6fad2045a47d2df8a878ac48337b3a5d66c652
waleolajumoke <waleolajumoke@gmail.com>
2024-09-10 14:12:25 +0100

    pushing the flask app to github

---
+++ app.py
@@ -0,0 +1,14 @@
+# generate a basic flask api
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/') # define the route
+def hello_world():
+    return 'welcome to Python'
+
+if __name__ == '__main__':
+    app.run(port=5000)
+    
+# run the app with python app.py
+# go to http://127.0.0.1:5000/ to see the output
+# to stop the app, press ctrl+c
